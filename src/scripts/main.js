ce784553a1cdb7950358517abfbeb8de4b88406a
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-05-20 14:26:32 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write code here
+const thElements = document.querySelectorAll('th');
+
+thElements.forEach((th) => {
+  th.addEventListener('click', (e) => {
+    const index = Array.from(thElements).indexOf(e.target);
+    const rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    rows.sort((a, b) => {
+      const cellA = a.children[index].textContent.trim();
+      const cellB = b.children[index].textContent.trim();
+
+      const cleanCellA = cellA.replace(/[^0-9.-]+/g, '');
+      const cleanCellB = cellB.replace(/[^0-9.-]+/g, '');
+
+      const numA = Number(cleanCellA);
+      const numB = Number(cleanCellB);
+
+      if (!isNaN(numA) && !isNaN(numB)) {
+        return numA - numB;
+      } else {
+        return cellA.localeCompare(cellB);
+      }
+    });
+
+    const tbody = document.querySelector('tbody');
+
+    tbody.innerHTML = '';
+
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
